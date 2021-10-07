9ed9f69466730add5e4c2327748425e4e5fbfc02
restu1909 <58680735+restu1909@users.noreply.github.com>
2021-10-07 10:24:47 +0700

    menyelesaikan latihan di video 4

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WPU blog | about</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
     <h1>halaman about</h1>
-    <h3><?=$name;?></h3>
-    <p><?=$email;?></p>
-    <img src="img/<?=$image;?>" alt="Muhammad Restu" width="200">
-</body>
-</html>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+@endsection
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,16 +14,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        "title" => "home"
+    ]);
 });
 Route::get('/about', function () {
     return view('about', [
+        "title" => "anout",
         "name" => "Muhammad Restu",
         "email" => "muhammadrestu1909@gmail.com",
         "image" => "unpas.png"
     ]);
 });
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "judul post pertama",
+            "slug" => "judul-post-pertama" ,
+            "author" => "Muhammad Restu",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio aperiam unde! Dolorem tempore ea natus, possimus beatae facere corporis saepe quo quaerat earum, eligendi consectetur iusto asperiores voluptas incidunt!"
+        ],
+        [
+            "title" => "judul post kedua",
+            "slug" => "judul-post-kedua" ,
+            "author" => "aji fattah",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio aperiam unde! Dolorem tempore ea natus, possimus beatae facere corporis saepe quo quaerat earum, eligendi consectetur iusto asperiores voluptas incidunt!"
+        ],
+    ];
+    
+    return view('posts',[
+        "title" => "posts",
+        "posts" => $blog_posts
+    ]);
 });
 
+
+
+route :: get('posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            "title" => "judul post pertama",
+            "slug" => "judul-post-pertama" ,
+            "author" => "Muhammad Restu",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio aperiam unde! Dolorem tempore ea natus, possimus beatae facere corporis saepe quo quaerat earum, eligendi consectetur iusto asperiores voluptas incidunt!"
+        ],
+        [
+            "title" => "judul post kedua",
+            "slug" => "judul-post-kedua" ,
+            "author" => "aji fattah",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio aperiam unde! Dolorem tempore ea natus, possimus beatae facere corporis saepe quo quaerat earum, eligendi consectetur iusto asperiores voluptas incidunt!"
+        ],
+    ];
+    $new_post =[];
+    foreach($blog_posts as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+    return view('post',[
+        "title" => "single post",
+        "post" => $new_post
+    ]);
+});
