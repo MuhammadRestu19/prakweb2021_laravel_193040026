9b24447a3283044bd77480ede4bd3d6572b1ee71
restu1909 <58680735+restu1909@users.noreply.github.com>
2021-10-28 11:24:23 +0700

    menyelesaikan latihan di video 2 view login&registration

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" href="/">Home</a>
+          <a class="nav-link {{ ($active === "h ome") ? 'active' : '' }}" href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/about">About</a>
@@ -19,6 +19,12 @@
           <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categpries</a>
         </li>
         
+      </ul>
+      <ul class="navbar-nav ms-auto">
+      <li class="nav-item">
+        <a href="/login " class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-right"></i>
+        Login</a>
+      </li>
       </ul>
     </div>
   </div>
